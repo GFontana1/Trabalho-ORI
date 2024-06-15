b6ec134e4394e2a8be9df951cdd6999a7c984a65
GFontana1 <gui.fontana0701@gmail.com>
2024-06-14 23:39:47 -0300

    test

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import javax.swing.*;
+
+public class App extends JFrame {
+    public App() {
+        setSize(600, 500);
+        setTitle("Trabalho ORI");
+        setLocationRelativeTo(new JFrame());   
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String args[]) {
+        new App();
+        try {
+            Connection c = DriverManager.getConnection("jdbc:sqlite:BD_Loja.db");
+            Statement stm = c.createStatement();
+            ResultSet rs = stm.executeQuery("SELECT * FROM Loja");
+            while ( rs.next() ) {
+               JOptionPane.showMessageDialog (
+                null, 
+                rs.getInt("id")+"\n" + 
+                rs.getString("nome")+"\n" +
+                rs.getString("telefone")+"\n" );
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        JOptionPane.showMessageDialog(null, "SQLite acessado com sucesso!");
+    }
+}
